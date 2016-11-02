fe7157e599f07e3a0cdffa00d711478e3c432238
Thomas Mutton <tmutton@businesssystemsuk.com>
2016-11-02 16:49:38 +0000

    init commit

---
+++ SharpCompressTarDirTest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpCompressTarDirTest
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SharpCompressTarDirTest");
+            Console.Read();
+        }
+    }
+}
